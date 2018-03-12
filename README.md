019a36613acc8d63cd16aa0f1deeb69cd48b42ff
PotentialOnWings <xiangyi99@live.com>
2018-03-12 17:35:57 -0400

    finished connecting video input, fairly accurate functional classifier, needs to finish returning the box coordinates

---
+++ README.md
@@ -0,0 +1,53 @@
+<img src=screenshots/starwars_small.gif width=100% />
+
+# Buoy Object Detection with TensorFlow's Mobilenet model
+Object detection allows for the recognition, detection, and localization of multiple buoys within an image. 
+
+## Installation
+
+Download Docker and run the dockerfile to install all the dependencies. Folder structure should be like this:
+
+```bash
+dockerimagename/Dockerfile
+```
+Run inside the dockerimagename folder. 
+
+```bash
+sudo docker build -t  dockerimagename . 
+```
+
+Check if the docker image has been build successfully with 
+
+```bash
+sudo docker image ls
+```
+
+Start the docker with: (change to appropriate paths)
+
+```bash
+xhost +
+```
+
+```bash
+sudo docker run -it --privileged -p 8888:8888 --env DISPLAY=$DISPLAY -v /dev/video0:/dev/video0 --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v /home/path/to/cloned/repo/:/home/TF dockerimagename:latest
+```
+
+Inside the docker, add `models` and `models/slim` to your `PYTHONPATH` using:
+
+```bash
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+```
+
+>_**Note:** This last command must be ran every time you open terminal, or added to your `~/.bashrc` file._
+
+
+## Usage
+Run:
+
+```bash
+python object_detection/object_detection_runner.py
+```
+
+## Source
+
+[MIT](LICENSE)
---
+++ object_detection/object_detection_runner.py
@@ -0,0 +1,247 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import json
+import time
+import glob
+
+from io import StringIO
+from PIL import Image
+
+import matplotlib.pyplot as plt
+
+from utils import visualization_utils as vis_util
+from utils import label_map_util
+
+from multiprocessing.dummy import Pool as ThreadPool
+
+
+
+##############more##############
+import yaml
+import cv2
+from stuff.helper import FPS2, WebcamVideoStream
+from tensorflow.core.framework import graph_pb2
+
+## OG PARAMETERS ##
+MAX_NUMBER_OF_BOXES = 10
+MINIMUM_CONFIDENCE = 0.9
+
+PATH_TO_LABELS = 'annotations/label_map.pbtxt'
+PATH_TO_TEST_IMAGES_DIR = 'test_images'
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=sys.maxsize, use_display_name=True)
+CATEGORY_INDEX = label_map_util.create_category_index(categories)
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+MODEL_NAME = 'output_inference_graph'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+
+
+
+## LOAD CONFIG PARAMS ##
+# with open("config.yml", 'r') as ymlfile:
+#     cfg = yaml.load(ymlfile)
+
+# video_input         = cfg['video_input']
+# visualize           = cfg['visualize']
+# vis_text            = cfg['vis_text']
+# max_frames          = cfg['max_frames']
+# width               = cfg['width']
+# height              = cfg['height']
+# fps_interval        = cfg['fps_interval']
+# allow_memory_growth = cfg['allow_memory_growth']
+# det_interval        = cfg['det_interval']
+# det_th              = cfg['det_th']
+# model_name          = cfg['model_name']
+# model_path          = cfg['model_path']
+# label_path          = cfg['label_path']
+# num_classes         = cfg['num_classes']
+# # split_model         = cfg['split_model']
+# log_device          = cfg['log_device']
+# #ssd_shape           = cfg['ssd_shape']
+
+video_input =  0              # Input Must be OpenCV readable 
+visualize = True             # Disable for performance increase
+vis_text = True              # Display fps on visualization stream
+max_frames = 500             # only used if visualize==False
+width = 300                  # 300x300 is used by SSD_Mobilenet -> highest fps
+height = 300
+fps_interval = 3             # Intervall [s] to print fps in console
+det_interval = 75            # intervall [frames] to print detections to console
+det_th = 0.5                 # detection threshold for det_intervall
+split_model = False           # Splits Model into a GPU and CPU session (currently only works for ssd_mobilenets)
+log_device = False            # Logs GPU / CPU device placement
+allow_memory_growth = True   # limits memory allocation to the actual needs
+
+
+model_name = 'ssd_mobilenet_v1_coco'
+model_path = 'output_inference_graph/frozen_inference_graph.pb'
+label_path = 'annotations/label_map.pbtxt'
+num_classes = 1
+#######################################################
+
+
+#do not need
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+def detect_objects(image_path):
+    image = Image.open(image_path)
+    image_np = load_image_into_numpy_array(image)
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+
+    (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections], feed_dict={image_tensor: image_np_expanded})
+
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        CATEGORY_INDEX,
+        min_score_thresh=MINIMUM_CONFIDENCE,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    fig = plt.figure()
+    fig.set_size_inches(16, 9)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+    plt.imshow(image_np, aspect = 'auto')
+    plt.savefig('output/{}'.format(image_path), dpi = 62)
+    plt.close(fig)
+
+
+def length_of_bounding_box(bbox):
+    print(bbox[0][3]*IMG_WIDTH - bbox[0][1]*IMG_WIDTH)
+
+def object_detection(video_input,visualize,max_frames,width,height,fps_interval,bbox_thickness, \
+                     allow_memory_growth,det_intervall,det_th,model_name):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth=allow_memory_growth
+        
+
+    cur_frames = 0
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph, config = config) as sess:
+            # Definite input and output Tensors for detection_graph
+            image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+            # Each box represents a part of the image where a particular object was detected.
+            detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+            # Each score represent how level of confidence for each of the objects.
+            # Score is shown on the result image, together with the class label.
+            detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+            detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+            num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+            # fps calculation
+            fps = FPS2(fps_interval).start()
+            # Start Video Stream
+            video_stream = WebcamVideoStream(video_input,width,height).start()
+            print ("Press 'q' to Exit")
+            while video_stream.isActive():
+                image_np = video_stream.read()
+                # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+                image_np_expanded = np.expand_dims(image_np, axis=0)
+                # Actual detection.
+                (boxes, scores, classes, num) = sess.run([detection_boxes, detection_scores, detection_classes, num_detections], feed_dict={image_tensor: image_np_expanded})
+                if visualize:
+                    # Visualization of the results of a detection.
+                    vis_util.visualize_boxes_and_labels_on_image_array(
+                    image_np,
+                    np.squeeze(boxes),
+                    np.squeeze(classes).astype(np.int32),
+                    np.squeeze(scores),
+                    CATEGORY_INDEX,
+                    min_score_thresh=MINIMUM_CONFIDENCE,
+                    use_normalized_coordinates=True,
+                    line_thickness=bbox_thickness)
+                    cv2.imshow('object_detection', image_np)
+                    
+                    # length_of_bounding_box(boxes)
+
+                    # Exit Option--BROKEN
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+                else:
+                    cur_frames += 1
+                    for box, score, _class in zip(np.squeeze(boxes), np.squeeze(scores), np.squeeze(classes)):
+                        if cur_frames%det_intervall==0 and score > det_th:
+                            label = category_index[_class]['name']
+                            print(label, score, box)
+                    if cur_frames >= max_frames:
+                        break
+                # fps calculation
+                fps.update()
+    
+    # End everything
+    fps.stop()
+    video_stream.stop()     
+    cv2.destroyAllWindows()
+    print('[INFO] elapsed time (total): {:.2f}'.format(fps.elapsed()))
+    print('[INFO] approx. FPS: {:.2f}'.format(fps.fps()))
+
+   
+
+        
+    # fig = plt.figure()
+    # fig.set_size_inches(16, 9)
+    # ax = plt.Axes(fig, [0., 0., 1., 1.])
+    # ax.set_axis_off()
+    # fig.add_axes(ax)
+
+    # plt.imshow(image_np, aspect = 'auto')
+    # plt.savefig('output/{}'.format(image_path), dpi = 62)
+    # plt.close(fig)
+
+
+
+
+# TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image-{}.jpg'.format(i)) for i in range(1, 4) ]
+TEST_IMAGE_PATHS = glob.glob(os.path.join(PATH_TO_TEST_IMAGES_DIR, '*.jpg'))
+
+# Load model into memory
+print('Loading model...')
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+print('detecting...')
+with detection_graph.as_default():
+    with tf.Session(graph=detection_graph) as sess:
+        image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+
+        # for image_path in TEST_IMAGE_PATHS:
+        #     detect_objects(image_path)
+
+
+    model_name = 'ssd_mobilenet_v1_coco'
+    video_input = 0              # Input Must be OpenCV readable 
+    visualize = True             # Disable for performance increase
+    max_frames = 500             # only used if visualize==False
+    width = 300                  # 300x300 is used by SSD_Mobilenet -> highest fps
+    height = 300
+    fps_interval = 3             # Intervall [s] to print fps in console
+    bbox_thickness = 8           # thickness of bounding boxes printed to the output image
+    allow_memory_growth = True   # restart python to apply changes on memory usage
+    det_intervall = 75           # intervall [frames] to print detections to console
+    det_th = 0.5                 # detection threshold for det_intervall
+    
+
+    object_detection(video_input, visualize, max_frames, width, height, fps_interval, bbox_thickness, allow_memory_growth, det_intervall, det_th, model_name)
